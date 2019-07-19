39948e40e7a84b90fbdc53af2e803a37c662d240
noah Branch <nbranch@vols.utk.edu>
2019-07-18 20:14:47 -0400

    this require export seems to work???

---
+++ electron_app/my/requiretest.js
@@ -0,0 +1,57 @@
+var firebase = require('firebase/app')
+  //require('firebase/firestore')
+require('firebase/database')
+require('firebase/auth')
+
+var test = require('./my/signin')
+
+
+test()
+
+///////////////////////////
+firebase.auth().onAuthStateChanged(user =>{
+    if(user){
+        console.log(user.email)
+        console.log("we in there")
+        window.location.replace("shop.html")
+    }else{
+        console.log("not signed in")
+    }
+})
+
+signin.addEventListener('click', function(){
+    var email = document.getElementById('usersignin').value;
+    var password = document.getElementById('passwordsignin').value;
+
+    console.log('signing in')
+    console.log(email)
+    console.log(password)
+
+    firebase.auth().signInWithEmailAndPassword(email,password)
+    .then(
+        function(dauser){
+            console.log("aye bobby!")
+        })
+    .catch(
+        function(error) {
+            console.log("ERROR BIH")
+        })
+        /*.catch(function(error) {
+            var ec = error.code
+            console.log("there was an error")
+            console.log(ec)
+            console.log(error.message)
+        }).then((user)=>{console.log("gotemm"); console.log(user.email)})*/
+
+    firebase.auth().onAuthStateChanged(user =>{
+        if(user){
+            console.log(user.email)
+            console.log("we in there")
+            window.location.replace("shop.html")
+        }else{
+            console.log("not signed in")
+        }
+    })
+
+const signin = document.getElementById('signin')
+})
--- electron_app/my/signin.js
+++ electron_app/my/signin.js
@@ -11,77 +11,78 @@ console.log("testing1")
   //var ipc = require('electron').ipcRenderer;
   //var elec = require("./main")
 
-module.exports = { 
-hello: function(){
-    if(firebase.apps.length ===0){
-        var firebaseConfig = {
-            apiKey: "AIzaSyAu7LfoTi5W2O7c_xAM49-dGN4_ZYf4ROk",
-            authDomain: "wodiver.firebaseapp.com",
-            databaseURL: "https://wodiver.firebaseio.com",
-            projectId: "wodiver",
-            storageBucket: "wodiver.appspot.com",
-            messagingSenderId: "1009484581983",
-            appId: "1:1009484581983:web:f8e0afe03d0490de"
+//module.exports = { 
+    hello = function(){
+        if(firebase.apps.length ===0){
+            var firebaseConfig = {
+                apiKey: "AIzaSyAu7LfoTi5W2O7c_xAM49-dGN4_ZYf4ROk",
+                authDomain: "wodiver.firebaseapp.com",
+                databaseURL: "https://wodiver.firebaseio.com",
+                projectId: "wodiver",
+                storageBucket: "wodiver.appspot.com",
+                messagingSenderId: "1009484581983",
+                appId: "1:1009484581983:web:f8e0afe03d0490de"
             };
-            // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
+                // Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
         }
-        console.log("testing exports")
+            console.log("testing exports")
     }
-};
+//};
 
-hello()
-
-
+module.exports = hello
 
 
 
 
-const signin = document.getElementById('signin')
-///////////////////////////
-firebase.auth().onAuthStateChanged(user =>{
-    if(user){
-        console.log(user.email)
-        console.log("we in there")
-        window.location.replace("shop.html")
-    }else{
-        console.log("not signed in")
-    }
-})
-
-signin.addEventListener('click', function(){
-    var email = document.getElementById('usersignin').value;
-    var password = document.getElementById('passwordsignin').value;
-
-    console.log('signing in')
-    console.log(email)
-    console.log(password)
-
-    firebase.auth().signInWithEmailAndPassword(email,password)
-    .then(
-        function(dauser){
-            console.log("aye bobby!")
-        })
-    .catch(
-        function(error) {
-            console.log("ERROR BIH")
-        })
-        /*.catch(function(error) {
-            var ec = error.code
-            console.log("there was an error")
-            console.log(ec)
-            console.log(error.message)
-        }).then((user)=>{console.log("gotemm"); console.log(user.email)})*/
-
-    firebase.auth().onAuthStateChanged(user =>{
-        if(user){
-            console.log(user.email)
-            console.log("we in there")
-            window.location.replace("shop.html")
-        }else{
-            console.log("not signed in")
-        }
-    })
 
 
-})
+
+
+// ///////////////////////////
+// firebase.auth().onAuthStateChanged(user =>{
+//     if(user){
+//         console.log(user.email)
+//         console.log("we in there")
+//         window.location.replace("shop.html")
+//     }else{
+//         console.log("not signed in")
+//     }
+// })
+
+// signin.addEventListener('click', function(){
+//     var email = document.getElementById('usersignin').value;
+//     var password = document.getElementById('passwordsignin').value;
+
+//     console.log('signing in')
+//     console.log(email)
+//     console.log(password)
+
+//     firebase.auth().signInWithEmailAndPassword(email,password)
+//     .then(
+//         function(dauser){
+//             console.log("aye bobby!")
+//         })
+//     .catch(
+//         function(error) {
+//             console.log("ERROR BIH")
+//         })
+//         /*.catch(function(error) {
+//             var ec = error.code
+//             console.log("there was an error")
+//             console.log(ec)
+//             console.log(error.message)
+//         }).then((user)=>{console.log("gotemm"); console.log(user.email)})*/
+
+//     firebase.auth().onAuthStateChanged(user =>{
+//         if(user){
+//             console.log(user.email)
+//             console.log("we in there")
+//             window.location.replace("shop.html")
+//         }else{
+//             console.log("not signed in")
+//         }
+//     })
+
+// const signin = document.getElementById('signin')
+// })
